d38f7764fe1b26c189e5d785b58a65f2e8bb4375
Charles Morin <charles.mor@outlook.fr>
2023-07-21 11:53:56 -0400

    Removed proto files

--- index.ts
+++ index.ts
@@ -3,7 +3,6 @@ import { handleOperations } from "./src/metrics.js";
 import pkg from "./package.json" assert { type: "json" };
 
 export * from "./src/metrics.js";
-export * from "./src/generated/pinax/substreams/sink/prometheus/v1/prometheus_pb.js";
 
 export async function action(options: commander.RunOptions) {
     const { emitter } = await setup(options, pkg);
