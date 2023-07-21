7e3549b1065601087ff5f2675d8ff4cc316d96da
Charles Morin <charles.mor@outlook.fr>
2023-07-21 13:16:12 -0400

    Added proto types back

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { handleOperations } from "./src/metrics.js";
 import pkg from "./package.json" assert { type: "json" };
 
 export * from "./src/metrics.js";
+export * from "./src/generated/pinax/substreams/sink/prometheus/v1/prometheus_pb.js";
 
 export async function action(options: commander.RunOptions) {
     const { emitter } = await setup(options, pkg);
