b9cbd277a31ea168afd4dc5aebe4195f0cf9da1b
Charles Morin <charles.mor@outlook.fr>
2023-09-18 16:37:06 -0400

    Bumped up substreams-sink version

--- index.ts
+++ index.ts
@@ -1,12 +1,11 @@
 import { setup, commander, http } from "substreams-sink";
 import { handleOperations } from "./src/metrics.js";
-import pkg from "./package.json" assert { type: "json" };
 
 export * from "./src/metrics.js";
 export * from "./src/generated/pinax/substreams/sink/prometheus/v1/prometheus_pb.js";
 
 export async function action(options: commander.RunOptions) {
-    const { emitter } = await setup(options, pkg);
+    const { emitter } = await setup(options);
     emitter.on("anyMessage", message => {
         handleOperations(message);
     });
